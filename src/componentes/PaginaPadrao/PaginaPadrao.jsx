8fa9a097d6b165d0fd7666e7ea0f4b218d944680
Gravatinha <gleberphant@gmail.com>
2026-02-03 07:36:36 -0300

    fix: Remove componente Banner da PaginaPadrao

--- src/componentes/PaginaPadrao/PaginaPadrao.jsx
+++ src/componentes/PaginaPadrao/PaginaPadrao.jsx
@@ -1,10 +1,9 @@
 import { Outlet } from 'react-router-dom'
-import Banner from 'componentes/Banner/Banner'
 
 export default function PaginaPadrao(){
     return(
         <main>
-            <Banner />
+            
             <Outlet />
         </main>
     )
